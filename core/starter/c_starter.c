ddd454265b0628138434c802b9958f6ea1ce0d2d
hp100.net <whitewest87@gmail.com>
2015-02-06 13:21:17 +0900

    준석이껄 참고해서 컴파일-> 실행하는 코드 수정함.

--- core/starter/c_starter.c
+++ core/starter/c_starter.c
@@ -1,15 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int system(const char *string);
-
-void main(int argc, char *argv[]){
-		char p[100];//path
-		char *DIRECTORY="dir";       //시스템명령어 dir
-		//strcpy(p,argv[0]);
-        strcpy(p,"aaa_bbb.c");
-		system("gcc -o aaa_bbb.out ./result/aaa_bbb.c");
-		system("./aaa_bbb.out");
-		
-}        
+int main(int argc, char* argv[]){
+	char path[400];
+	char cmd_compile[400];
+	char cmd_exec[400];
+	if(argc < 2){
+		printf("no input source \n");
+	}
+	sprintf(path,"../generate/%s",argv[1]);
+	sprintf(cmd_compile,"gcc -o ./result/%s.out %s",argv[1],path);
+	system(cmd_compile);
+	
+	sprintf(cmd_exec,"./result/%s.out",argv[1]);
+	system(cmd_exec);
+	return 0;
+}
