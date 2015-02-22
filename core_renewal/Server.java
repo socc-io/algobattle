bcce08e1af52030dfe7e95ad5ac50bb4de583300
hp100.net <whitewest87@gmail.com>
2015-02-22 18:36:29 +0900

    Update Server.java

--- core_renewal/Server.java
+++ core_renewal/Server.java
@@ -1,11 +1,42 @@
 
+class GameFrameWork{
+    public void start(){
+        init();
+        init_packet_transfer();
+        server_start();
+        while(true){
+            valid();
+            play();
+            history();
+            packet_transfer();
+        }
+    }
+    public abstract void valid();
+    public abstract void play();
+    public abstract void history();
+    public abstract void packet_tranfer();
+}
 
+class TicTaktoe extends GameFramework{
+    
+}
 
+class GameFactory {
+    public getGame(String title){
+        GameFrameWork gfw = null;
+        if(title.equals("Tictaktoe"){
+            gfw = new Tictaktoe();
+        }
+        return gfw;
+    }   
+}
 
 public class Server{
-
     public static void main(String[] ar){
-            
-    
+        GameFactory gf = new GameFactory();
+        GameFrameWork gfw = gf.getGame(ar[1]);
+        gfw.start();
     }
 }
+
+// java Server TikTacktoe
